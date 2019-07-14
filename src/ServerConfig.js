3f0a91c2ad1e8b1e1dad7d2b1987c5b0104ba617
nicovapa <upebv@student.kit.edu>
2019-07-14 17:06:11 +0200

    chaged server IP

--- src/ServerConfig.js
+++ src/ServerConfig.js
@@ -1,2 +1,2 @@
 //constant address of the server
-export const SERVER = 'http://193.196.36.83:8443' // IP + port of the host, or  http://localhost:8443 for testing on pc
+export const SERVER = 'http://193.196.38.69:8443' // IP + port of the host, or  http://localhost:8443 for testing on pc
